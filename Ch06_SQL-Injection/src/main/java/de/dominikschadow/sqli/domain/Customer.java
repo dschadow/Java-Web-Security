a3090782d9718202c81bba3b07928dac612b5385
Dominik Schadow <dominikschadow@googlemail.com>
2012-11-25 16:05:13 +0100

    Initial SQL Injection sample project
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

---
+++ Ch06_SQL-Injection/src/main/java/de/dominikschadow/sqli/domain/Customer.java
@@ -0,0 +1,51 @@
+package de.dominikschadow.sqli.domain;
+
+public class Customer {
+    private int custId;
+    private String name;
+    private String status;
+    private int orderLimit;
+
+    public int getCustId() {
+        return custId;
+    }
+
+    public void setCustId(int custId) {
+        this.custId = custId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getOrderLimit() {
+        return orderLimit;
+    }
+
+    public void setOrderLimit(int orderLimit) {
+        this.orderLimit = orderLimit;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder customer = new StringBuilder();
+        customer.append("ID ").append(custId);
+        customer.append("Name ").append(name);
+        customer.append("Status ").append(status);
+        customer.append("Order limit").append(orderLimit);
+        
+        return customer.toString();
+    }
+}
