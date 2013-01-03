c5083e5d7a15f140aa9f0c307077f85fd270773d
Dominik Schadow <dominikschadow@googlemail.com>
2013-01-03 16:20:50 +0100

    Added sample injected data
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- Ch07_XSS/src/main/resources/import.sql
+++ Ch07_XSS/src/main/resources/import.sql
@@ -3,3 +3,4 @@ INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBE
 INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Hammel','C',1000,1);
 INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Gretchen','D',500,1);
 INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Maier','A',100000,1);
+INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'<script>alert("Cookie " + document.cookie)</script>','A',100000,1);
