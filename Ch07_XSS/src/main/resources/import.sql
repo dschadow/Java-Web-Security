265ec1ce9a8f2ce7f17cd1872e5823c450876ef0
Dominik Schadow <dominikschadow@googlemail.com>
2013-01-03 09:59:53 +0100

    Create/ insert for customer table
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

---
+++ Ch07_XSS/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+CREATE TABLE customer (cust_id integer, name varchar(45), status varchar(45), order_limit integer);
+
+INSERT INTO customer VALUES (1,'Müller','A',10000);
+INSERT INTO customer VALUES (2,'Kaiser','B',5000);
+INSERT INTO customer VALUES (3,'Hammel','C',1000);
+INSERT INTO customer VALUES (4,'Gretchen','D',500);
+INSERT INTO customer VALUES (5,'Maier','A',100000);
