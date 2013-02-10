31accc09ec11dbbd6199daea70b7d4e96522a4d8
Dominik Schadow <dominikschadow@googlemail.com>
2013-02-10 15:10:46 +0100

    CSRF sample
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

---
+++ Ch08_CSRF/src/main/Java/de/dominikschadow/webappsecurity/CSRFPostServlet.java
@@ -0,0 +1,41 @@
+package de.dominikschadow.webappsecurity;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CSRFPostServlet
+ */
+public class CSRFPostServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CSRFPostServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        System.out.println("Processing post request");
+        System.out.println("Received " + request.getParameter("name") + " as form parameter");
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<p>Received " + request.getParameter("name") + " as form parameter</p>");
+        out.println("</body></html>");
+        out.flush();
+        out.close();
+    }
+}
---
+++ Ch08_CSRF/src/main/webapp/index.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Cross-Site Request Forgery</title>
+</head>
+<body>
+	<h1>Cross-Site Request Forgery</h1>
+
+	<form name="Login" method="post" action="CSRFPostServlet">
+		<table>
+			<tr>
+				<td>Name</td>
+				<td><input type="text" name="name"></td>
+				<td><input type="submit" value="Senden"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
