3fa381a490f7b112a7b616990b51211152c43282
Dominik Schadow <dominikschadow@googlemail.com>
2013-03-20 21:38:00 +0100

    New names for customers
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- Ch07_XSS/src/main/resources/import.sql
+++ Ch07_XSS/src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Müller','A',10000,1);
-INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Kaiser','B',5000,1);
-INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Hammel','C',1000,1);
-INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Gretchen','D',500,1);
-INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Maier','A',100000,1);
+INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Arthur Dent','A',10000,1);
+INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Ford Prefect','B',5000,1);
+INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Tricia Trillian McMillan','C',1000,1);
+INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Zaphod Beeblebrox','D',500,1);
+INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Marvin','A',100000,1);
 INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'<script>alert("Cookie " + document.cookie)</script>','A',100000,1);
