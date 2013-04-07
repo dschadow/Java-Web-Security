2ef86c1f9b72a999f12fec241cb30b2960b08685
Dominik Schadow <dominikschadow@googlemail.com>
2013-04-07 16:34:02 +0200

    Updated sample project for AccessReferenceMap
    IntegerAccessReferenceMap and RandomAccessReferenceMap
    
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

---
+++ Ch05_AccessReferenceMaps/src/main/java/de/dominikschadow/webappsecurity/User.java
@@ -0,0 +1,22 @@
+package de.dominikschadow.webappsecurity;
+
+public class User {
+    private int accountId;
+    private String name;
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
