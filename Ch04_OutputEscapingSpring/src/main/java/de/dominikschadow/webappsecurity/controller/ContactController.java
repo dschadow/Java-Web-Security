abb8f3c92898eae626fb5b79dc268b9707707c97
Dominik Schadow <dominikschadow@googlemail.com>
2013-05-19 20:29:56 +0200

    Added Javadoc and license information
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- Ch04_OutputEscapingSpring/src/main/java/de/dominikschadow/webappsecurity/controller/ContactController.java
+++ Ch04_OutputEscapingSpring/src/main/java/de/dominikschadow/webappsecurity/controller/ContactController.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2013 Dominik Schadow, dominikschadow@gmail.com
+ *
+ * This file is part of JavaWebAppSecurity.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.dominikschadow.webappsecurity.controller;
 
 import org.springframework.stereotype.Controller;
@@ -10,6 +27,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 import de.dominikschadow.webappsecurity.domain.Contact;
 
+/**
+ * 
+ * @author Dominik Schadow
+ */
 @Controller
 @SessionAttributes
 public class ContactController {
