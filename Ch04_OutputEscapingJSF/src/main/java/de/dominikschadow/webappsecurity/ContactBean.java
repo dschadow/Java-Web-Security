c0b1e19bc24edaadfd1319434be5cae98b501338
Dominik Schadow <dominikschadow@googlemail.com>
2013-09-15 18:57:25 +0200

    Added Output-Escaping sample project with Java Server Faces

---
+++ Ch04_OutputEscapingJSF/src/main/java/de/dominikschadow/webappsecurity/ContactBean.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2013 Dominik Schadow, dominikschadow@gmail.com
+ *
+ * This file is part of the Java Security Myths project.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package main.java.de.dominikschadow.webappsecurity;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * Bean representing user input (no additional security related attributes).
+ * 
+ * @author Dominik Schadow
+ */
+@ManagedBean(name = "contact")
+@SessionScoped
+public class ContactBean implements Serializable {
+    private static final long serialVersionUID = 4083596061570021965L;
+
+    private String firstname;
+    private String lastname;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
