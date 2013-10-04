4f331644b4e5527035a051c5e38805443d7e6a48
Dominik Schadow <dominikschadow@googlemail.com>
2013-10-04 21:39:22 +0200

    Updated sample

--- Ch06_SQLInjection/src/main/java/de/dominikschadow/sqli/servlets/StatementServlet.java
+++ Ch06_SQLInjection/src/main/java/de/dominikschadow/sqli/servlets/StatementServlet.java
@@ -17,6 +17,8 @@
  */
 package de.dominikschadow.sqli.servlets;
 
+import de.dominikschadow.sqli.domain.Customer;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,6 +26,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -33,12 +38,19 @@ import java.io.PrintWriter;
 @WebServlet(name = "StatementServlet", urlPatterns = {"/StatementServlet"})
 public class StatementServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private Connection con = null;
 
     /**
      * @see javax.servlet.http.HttpServlet#HttpServlet()
      */
     public StatementServlet() {
         super();
+
+        try {
+            con = DriverManager.getConnection("jdbc:hsqldb:file:src/main/resources/customerDB; shutdown=true", "sa", "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -48,11 +60,67 @@ public class StatementServlet extends HttpServlet {
         String name = request.getParameter("name");
         System.out.println("Received " + name + " as POST parameter");
 
+        String query = "SELECT * FROM customer WHERE name = '" + name + "' ORDER BY CUST_ID";
+        List<Customer> customers = new ArrayList<>();
+
+        System.out.println("Final SQL query " + query);
+
+        Statement stmt = null;
+
+        try {
+            stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                Customer customer = new Customer();
+                customer.setCustId(rs.getInt(1));
+                customer.setName(rs.getString(2));
+                customer.setStatus(rs.getString(3));
+                customer.setOrderLimit(rs.getInt(4));
+
+                customers.add(customer);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
         response.setContentType("text/html");
 
         try (PrintWriter out = response.getWriter()) {
+            out.println("<html>");
+            out.println("<head><link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\" /></head>");
+            out.println("<body>");
             out.println("<h1>Ch06_SQLInjection - Statement</h1>");
             out.println("<p><strong>Input was </strong> " + name + "</p>");
+            out.println("<h2>Customer Data</h2>");
+            out.println("<table>");
+            out.println("<tr>");
+            out.println("<th>ID</th>");
+            out.println("<th>Name</th>");
+            out.println("<th>Status</th>");
+            out.println("<th>Order Limit</th>");
+            out.println("</tr>");
+
+            for (Customer customer : customers) {
+                out.println("<tr>");
+                out.println("<td>" + customer.getCustId() + "</td>");
+                out.println("<td>" + customer.getName() + "</td>");
+                out.println("<td>" + customer.getStatus() + "</td>");
+                out.println("<td>" + customer.getOrderLimit() + "</td>");
+                out.println("</tr>");
+            }
+
+            out.println("<table>");
+            out.println("</body>");
+            out.println("</html>");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
