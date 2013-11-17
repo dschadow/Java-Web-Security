63162269f1c0efa1cb2fc4d9dfb33d9e324e63e6
Dominik Schadow <dominikschadow@googlemail.com>
2013-11-17 13:51:43 +0100

    Added two servlets for unescaped and escaped XPath expression processing

---
+++ Ch06_XPathInjection/src/main/java/de/dominikschadow/webappsecurity/servlets/XPathServlet.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2013 Dominik Schadow, dominikschadow@gmail.com
+ *
+ * This file is part of Java-Web-Security
+.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.dominikschadow.webappsecurity.servlets;
+
+import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet using an XPath expression to query the customer XML document.
+ * User input is not modified and used directly in the XPath expression.
+ * <p/>
+ * Use <code>' or '1' = '1</code> or <code>'] | /* | /foo[bar='</code> as password.
+ *
+ * @author Dominik Schadow
+ */
+@WebServlet(name = "XPathServlet", urlPatterns = {"/XPathServlet"})
+public class XPathServlet extends HttpServlet {
+    private static final Logger LOGGER = Logger.getLogger(XPathServlet.class);
+    private static final long serialVersionUID = 1L;
+    private Document doc;
+
+    @PostConstruct
+    public void init() {
+        try {
+            File xmlFile = new File("src/main/resources/customer.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            doc = dBuilder.parse(xmlFile);
+        } catch (SAXException | IOException | ParserConfigurationException ex) {
+            LOGGER.error(ex.getMessage(), ex);
+        }
+    }
+
+    /**
+     * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+        LOGGER.info("Received " + name + " and " + password + " as parameter");
+
+        StringBuilder xpathExpression = new StringBuilder();
+        xpathExpression.append("/customers/customer[name='");
+        xpathExpression.append(name);
+        xpathExpression.append("' and @password='");
+        xpathExpression.append(password);
+        xpathExpression.append("']/orderLimit");
+
+        printOrderLimit(xpathExpression.toString(), name, response);
+    }
+
+    private void printOrderLimit(String xpath, String name, HttpServletResponse response) {
+        LOGGER.info("XPath expression is " + xpath);
+
+        try (PrintWriter out = response.getWriter()) {
+            XPathExpression expression = XPathFactory.newInstance().newXPath().compile(xpath);
+            Object result = expression.evaluate(doc, XPathConstants.NODESET);
+
+            response.setContentType("text/html");
+
+            out.println("<html>");
+            out.println("<head><link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\" /></head>");
+            out.println("<body>");
+            out.println("<h1>Ch06_XPathInjection</h1>");
+            out.println("<h2>Order limit for " + name + "</h2>");
+
+            NodeList nodes = (NodeList) result;
+            for (int i = 0; i < nodes.getLength(); i++) {
+                out.println("<p>" + nodes.item(i).getTextContent() + "</p>");
+            }
+
+            out.println("</body>");
+            out.println("</html>");
+        } catch (XPathExpressionException | IOException ex) {
+            LOGGER.error(ex.getMessage(), ex);
+        }
+    }
+}
