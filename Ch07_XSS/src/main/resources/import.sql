93f04114322d78557a83aac163f528f01aa28a7d
Dominik Schadow <dominikschadow@googlemail.com>
2013-12-15 16:54:35 +0100

    Removed unused files

--- Ch07_XSS/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Arthur Dent','A',10000,1);
-INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Ford Prefect','B',5000,1);
-INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Tricia Trillian McMillan','C',1000,1);
-INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Zaphod Beeblebrox','D',500,1);
-INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'Marvin','A',100000,1);
-INSERT INTO customer(id, name, status, orderlimit,version) VALUES (NEXTVAL('HIBERNATE_SEQUENCE'),'<script>alert("Cookie " + document.cookie)</script>','A',100000,1);
