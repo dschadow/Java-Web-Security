58f211e591970918e32be06a027969e891459dfb
Dominik Schadow <dominikschadow@googlemail.com>
2013-12-27 10:40:40 +0100

    Organized imports

--- Ch05_HSTS/src/main/java/de/dominikschadow/webappsecurity/filter/HSTSFilter.java
+++ Ch05_HSTS/src/main/java/de/dominikschadow/webappsecurity/filter/HSTSFilter.java
@@ -21,13 +21,8 @@ package de.dominikschadow.webappsecurity.filter;
 import org.apache.log4j.Logger;
 
 import javax.servlet.*;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Filter to add the <code>Strict-Transport-Security</code> header to every response.
