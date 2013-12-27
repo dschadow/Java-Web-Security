f295604cd830e83a75b7dcdba2d4f890b6bc1236
Dominik Schadow <dominikschadow@googlemail.com>
2013-12-27 10:46:53 +0100

    Fixed package

--- Ch04_OutputEscapingJSF/src/main/java/de/dominikschadow/webappsecurity/ContactBean.java
+++ Ch04_OutputEscapingJSF/src/main/java/de/dominikschadow/webappsecurity/ContactBean.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package main.java.de.dominikschadow.webappsecurity;
+package de.dominikschadow.webappsecurity;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
