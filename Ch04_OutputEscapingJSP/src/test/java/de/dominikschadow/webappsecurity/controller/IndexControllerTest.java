5c803d1033d9f9198a79c3f9a05b7c7990fba7fc
Dominik Schadow <dominikschadow@googlemail.com>
2015-01-02 14:08:56 +0100

    Added JUnit-tests for controllers

---
+++ Ch04_OutputEscapingJSP/src/test/java/de/dominikschadow/webappsecurity/controller/IndexControllerTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2015 Dominik Schadow, dominikschadow@gmail.com
+ *
+ * This file is part of Java-Web-Security
+.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.dominikschadow.webappsecurity.controller;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+/**
+ *
+ * @author Dominik Schadow
+ */
+public class IndexControllerTest {
+    @Test
+    public void testIndexPage() throws Exception {
+        IndexController controller = new IndexController();
+        MockMvc mockMvc = standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/")).andExpect(view().name("index"));
+    }
+}
