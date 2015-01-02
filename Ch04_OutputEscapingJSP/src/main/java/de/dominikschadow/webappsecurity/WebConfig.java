e3f3bb3bef95913c33639f47e95aad684b7cae65
Dominik Schadow <dominikschadow@googlemail.com>
2015-01-02 14:37:40 +0100

    Javadoc cleanup

--- Ch04_OutputEscapingJSP/src/main/java/de/dominikschadow/webappsecurity/WebConfig.java
+++ Ch04_OutputEscapingJSP/src/main/java/de/dominikschadow/webappsecurity/WebConfig.java
@@ -1,8 +1,7 @@
 /*
  * Copyright (C) 2015 Dominik Schadow, dominikschadow@gmail.com
  *
- * This file is part of Java-Web-Security
-.
+ * This file is part of the Java-Web-Security project.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
